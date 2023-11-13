64734c82726e774252f9c0c77933c97d674f0e56
Yuriy Mamaev (execut) <sexecut@gmail.com>
2023-11-13 18:41:06 +0300

    List keyword implementation

--- internal/api/list_keyword_v1.go
+++ internal/api/list_keyword_v1.go
@@ -4,12 +4,22 @@ import (
     "context"
     "github.com/execut/ozon-keyword-api/pkg/ozon-keyword-api"
     "github.com/rs/zerolog/log"
-    "google.golang.org/grpc/codes"
-    "google.golang.org/grpc/status"
 )
 
 func (o *ozonAPI) ListKeywordV1(ctx context.Context, req *ozon_keyword_api.ListKeywordV1Request) (*ozon_keyword_api.ListKeywordV1Response, error) {
     log.Debug().Msg("ListKeywordV1")
+    keywords, err := o.repo.List(ctx, 100, 0)
+    if err != nil {
+        return nil, err
+    }
 
-    return nil, status.Error(codes.Unimplemented, "ListKeywordV1 not implemented")
+    result := []*ozon_keyword_api.Keyword{}
+    for _, keyword := range keywords {
+        result = append(result, &ozon_keyword_api.Keyword{
+            Id:   keyword.ID,
+            Name: keyword.Name,
+        })
+    }
+
+    return &ozon_keyword_api.ListKeywordV1Response{Items: result}, nil
 }
